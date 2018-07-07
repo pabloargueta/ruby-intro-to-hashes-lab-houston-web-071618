6da148e704b4c1fa637d56043d0c6cfa7a6fbed3
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-07 21:50:08 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,9 @@ def monopoly_with_third_tier
 	  names: {},
 	  rent_in_dollars: {
 	    one_piece-owned: 25,
-	    two_pieces_owned: 50
+	    two_pieces_owned: 50,
+	    three_pieces_owned: 100,
+      four_pieces_owned: 200
 	  }
 	}
 		
