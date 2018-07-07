0b535fa5fb1a62f04e253345b061a378b3d9bf4c
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-07 21:59:30 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,6 +16,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
+  monolo
 	railroads = {
 	  pieces: 4,
 	  names: {},
@@ -25,7 +26,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-		railroads = {
+  monopoly = {
+		railroads: {
   	  pieces: 4,
   	  names: {
   	    reading_railroad: {},
@@ -44,7 +46,8 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-		railroads = {
+  monopoly = {
+		railroads: {
   	  pieces: 4,
   	  names: {
   	    reading_railroad: {
